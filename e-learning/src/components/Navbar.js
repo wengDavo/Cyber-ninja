8345be7d6a09401d7dcb594c518a148674215de4
toyosi <toyosiola@yaoo.com>
2024-05-14 12:41:28 -0300

    nav_username

--- e-learning/src/components/Navbar.js
+++ e-learning/src/components/Navbar.js
@@ -110,13 +110,13 @@ const NavBar = () => {
 const LoggedInView = ({ user }) => {
   return (
     <div>
-      <Navbar.Text className="text-white">Welcome {user.username}</Navbar.Text>
+      {/* <Navbar.Text className="text-white">Welcome {user.username}</Navbar.Text> */}
       <Link to="/private" style={{ textDecoration: "none", color: "inherit" }}>
         <button
           className="btn-medium bg-orange-50"
           style={{ marginLeft: "10px" }}
         >
-          Profile
+          {user.username}
         </button>
       </Link>
       <Link to="/logout" style={{ textDecoration: "none", color: "inherit" }}>
