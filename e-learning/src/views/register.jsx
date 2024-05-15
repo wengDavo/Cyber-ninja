8757c4723d8e13e758cda290fa902517de2bb323
toyosi <toyosiola@yaoo.com>
2024-05-15 10:15:23 -0300

    resgister tailwind

--- e-learning/src/views/register.jsx
+++ e-learning/src/views/register.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { register } from "../utils/auth";
 import { useNavigate } from "react-router-dom/dist";
 import { useAuthStore } from "../store/auth";
-// import "../components/styles/css/signup.css";
 import eye from "../components/assets/icons/eye.svg";
 import northE from "../components/assets/icons/north-e.svg";
 import Testimonials from "../components/Testimonials";
@@ -104,18 +103,24 @@ function Register() {
   return (
     <body>
       <NavBar />
-      <main id="mainn">
-        <section className="signup">
-          <div>
-            <p className="signup--title">Sign Up</p>
-            <p className="signup--msg">
+      <main className="md:grid md:grid-cols-2">
+        <section class="bg-white-97 border border-white-95 p-7 flex flex-col gap-8">
+          <div className="text-center">
+            <p class="font-semiBold text-3xl mb-4">Sign Up</p>
+            <p class="signup--msg text-sm text-grey-30">
               Create an account to unlock exclusive features.
             </p>
           </div>
           <div>
-            <form action="" className="signup--form" onSubmit={handleSubmit}>
-              <p className="signup--item">
-                <label htmlFor="email">Email</label>
+            <form
+              action=""
+              className="signup--form flex flex-col gap-4"
+              onSubmit={handleSubmit}
+            >
+              <p className="flex flex-col gap-2 relative">
+                <label htmlFor="email" className="text-sm leading-6">
+                  Email
+                </label>
                 <input
                   type="email"
                   name="email"
@@ -124,7 +129,7 @@ function Register() {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className={error && error.email ? "error" : ""}
+                  className="p-5 border border-white-95 placeholder:leading-6 placeholder:text-sm placeholder:capitalize"
                 />
                 {error && error.email && (
                   <span className="error-message">
@@ -132,9 +137,12 @@ function Register() {
                   </span>
                 )}
               </p>
-              <p className="signup--item">
-                <label htmlFor="">First Name</label>
+              <p className="flex flex-col gap-2 relative">
+                <label htmlFor="first_name" className="text-sm leading-6">
+                  First Name
+                </label>
                 <input
+                  className="p-5 border border-white-95 placeholder:leading-6 placeholder:text-sm placeholder:capitalize"
                   type="text"
                   name="first_name"
                   id="first_name"
@@ -144,9 +152,12 @@ function Register() {
                   required
                 />
               </p>
-              <p className="signup--item">
-                <label htmlFor="">Last Name</label>
+              <p className="flex flex-col gap-2 relative">
+                <label htmlFor="last_name" className="text-sm leading-6">
+                  Last Name
+                </label>
                 <input
+                  className="p-5 border border-white-95 placeholder:leading-6 placeholder:text-sm placeholder:capitalize"
                   type="text"
                   name="last_name"
                   id="last_name"
@@ -156,8 +167,10 @@ function Register() {
                   required
                 />
               </p>
-              <p className="signup--item">
-                <label htmlFor="">Username</label>
+              <p className="flex flex-col gap-2 relative">
+                <label htmlFor="username" className="text-sm leading-6">
+                  Username
+                </label>
                 <input
                   type="text"
                   name="username"
@@ -166,7 +179,7 @@ function Register() {
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                   required
-                  className={error && error.username ? "error" : ""}
+                  className="p-5 border border-white-95 placeholder:leading-6 placeholder:text-sm placeholder:capitalize"
                 />
                 {error && error.username && (
                   <span className="error-message">
@@ -174,79 +187,88 @@ function Register() {
                   </span>
                 )}
               </p>
-              <p className="signup--item">
-                <label htmlFor="">Password</label>
-                <div className="password-input-container">
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    id="password"
-                    placeholder="Enter your Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    className={error && error.password ? "error" : ""}
-                  />
-                  <i onClick={togglePasswordVisibility}>
-                    <img src={eye} alt="" />
-                  </i>
-                </div>
+              <p className="flex flex-col gap-2 relative">
+                <label htmlFor="password" className="text-sm leading-6">
+                  Password
+                </label>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  placeholder="Enter your Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="p-5 border border-white-95 placeholder:leading-6 placeholder:text-sm placeholder:capitalize"
+                />
+                <i
+                  className="absolute right-0"
+                  onClick={togglePasswordVisibility}
+                >
+                  <img src={eye} alt="" />
+                </i>
                 {error && error.password && (
                   <span className="error-message">
                     {error.password.join("  ")}
                   </span>
                 )}
               </p>
-              <p className="signup--item">
-                <label htmlFor="">Confirm Password</label>
-                <div className="password-input-container">
-                  <input
-                    type={showPassword2 ? "text" : "password"}
-                    name="password2"
-                    id="password2"
-                    placeholder="Enter Password again"
-                    value={password2}
-                    onChange={(e) => setPassword2(e.target.value)}
-                    required
-                    className={error && error.password2 ? "error" : ""}
-                  />
-                  <i onClick={togglePassword2Visibility}>
-                    <img src={eye} alt="" />
-                  </i>
-                </div>
+              <p className="flex flex-col gap-2 relative">
+                <label htmlFor="password2" className="text-sm leading-6">
+                  Confirm Password
+                </label>
+                <input
+                  className="p-5 border border-white-95 placeholder:leading-6 placeholder:text-sm placeholder:capitalize"
+                  type={showPassword2 ? "text" : "password"}
+                  name="password2"
+                  id="password2"
+                  placeholder="Enter Password again"
+                  value={password2}
+                  onChange={(e) => setPassword2(e.target.value)}
+                  required
+                />
+                <i
+                  className="absolute right-0"
+                  onClick={togglePassword2Visibility}
+                >
+                  <img src={eye} alt="" />
+                </i>
                 {error && error.password2 && (
                   <span className="error-message">
                     {error.password2.join("  ")}
                   </span>
                 )}
               </p>
-              <a href="" className="signup--policy">
+              <a href="" className="text-grey-40">
                 I agree with Terms of Use and Privacy Policy
               </a>
-              <button type="submit" className="white p-12 rad-6 bg-orange-50">
+              <button
+                type="submit"
+                className="text-abs-white p-3 rounded-regular bg-orange-50"
+              >
                 Sign Up
               </button>
             </form>
-            <div className="signup--or">
-              <p></p>
+            <div class="flex w-full justify-between items-center my-4 text-grey-60 gap-1 text-sm">
+              <p className="w-full h-[1px] bg-grey-60"></p>
               <p>OR</p>
-              <p></p>
+              <p className="w-full h-[1px] bg-grey-60"></p>
             </div>
-            <div className="to-login">
+            <div className="text-sm leading-6 flex gap-2 items-center justify-center">
               <p>ALready have an account? </p>
               <Link
                 to="/login"
-                style={{ textDecoration: "none", color: "inherit" }}
+                // style={{ textDecoration: "none", color: "inherit" }}
               >
                 Login
               </Link>
               <i>
-                <img src={northE} alt="" className="icon" />
+                <img src={northE} alt="" className="" />
               </i>
             </div>
           </div>
         </section>
-        <section className="testti">
+        <section>
           <Testimonials />
         </section>
       </main>
