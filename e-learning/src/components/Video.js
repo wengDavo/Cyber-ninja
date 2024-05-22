40920263baa32e8cddc788af96689fa5ecb65d5d
toyosi <toyosiola@yaoo.com>
2024-05-22 14:21:29 -0300

    lesson_content_ui

--- e-learning/src/components/Video.js
+++ e-learning/src/components/Video.js
@@ -1,24 +1,26 @@
 import React, { useContext, useEffect, useState } from "react";
 const Video = ({ selectedLessonId, lesson }) => {
-  useEffect(() => {
-    if (selectedLessonId) {
-      // Load the video based on the selected lesson ID
-      console.log("Selected Lesson ID:", selectedLessonId);
-    }
-  }, [selectedLessonId]);
+  // useEffect(() => {
+  //   if (selectedLessonId) {
+  //     // Load the video based on the selected lesson ID
+  //     console.log("Selected Lesson ID:", selectedLessonId);
+  //   }
+  // }, [selectedLessonId]);
 
   return (
     <section className="video p-3">
-      <video src=""></video>
+      {/* <video src=""></video> */}
       <div>
-        <div className="group topicc border border-white-95 rounded-regular flex flex-col gap-y-8 p-5 md:flex-row md:justify-between md:items-center hover:border hover:border-orange-80 hover:shadow-md">
-          <p className="topicc--title  m-auto leading-6 font-medium">
+        <h2 className="topicc--title m-auto leading-6 font-bold overflow-y-auto scrollbar-hidden">
+          {" "}
+          {lesson.title}
+        </h2>
+        <div className="overflow-y-scroll max-h-[500px] mx-auto group topicc border border-white-95 rounded-regular flex flex-col gap-y-8 p-5 md:flex-row md:justify-between md:items-center border-orange-80 shadow-md">
+          <p className=" m-auto leading-6 font-light overflow-y-auto scrollbar-hidden">
             {lesson.content}
           </p>
         </div>
       </div>
-
-      <h2>Selected Lesson: {selectedLessonId}</h2>
     </section>
   );
 };
--- e-learning/tailwind.config.js
+++ e-learning/tailwind.config.js
@@ -1,52 +1,63 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js, jsx, ts, tsx}"
-  ],
+  content: ["./src/**/*.{js, jsx, ts, tsx}"],
   theme: {
-    colors:{
-      abs:{
-        white:"#FFFFFF",
-        black:"#000000",
+    colors: {
+      abs: {
+        white: "#FFFFFF",
+        black: "#000000",
       },
-      orange:{
-        50:"#FF9500",
-        70:"#FFBF66",
-        55:"#FFCA80",
-        80:"#FFCA80",
-        90:"#FFEACC",
-        95:"#FFF4E5",
-        97:"#FFF9F0",
-        99:"#FFFDFA",
+      orange: {
+        50: "#FF9500",
+        70: "#FFBF66",
+        55: "#FFCA80",
+        80: "#FFCA80",
+        90: "#FFEACC",
+        95: "#FFF4E5",
+        97: "#FFF9F0",
+        99: "#FFFDFA",
       },
-      white:{
-        90:"#E4E4E7",
-        95:"#F1F1F3",
-        97:"#F7F7F8",
-        99:"#FCFCFD",
+      white: {
+        90: "#E4E4E7",
+        95: "#F1F1F3",
+        97: "#F7F7F8",
+        99: "#FCFCFD",
       },
-      grey:{
-        10:"#1A1A1A",
-        15:"#262626",
-        20:"#333333",
-        30:"#4C4C4D",
-        35:"#59595A",
-        40:"#656567",
-        60:"#98989A",
-        70:"#B3B3B3",
+      grey: {
+        10: "#1A1A1A",
+        15: "#262626",
+        20: "#333333",
+        30: "#4C4C4D",
+        35: "#59595A",
+        40: "#656567",
+        60: "#98989A",
+        70: "#B3B3B3",
       },
     },
-    fontWeight:{
-      regular:"300",
-      medium:"500",
-      semiBold:"600",
-      bold:"700",
-      extraBold:"900",
+    fontWeight: {
+      regular: "300",
+      medium: "500",
+      semiBold: "600",
+      bold: "700",
+      extraBold: "900",
     },
-    borderRadius:{
-      regular:"6px",
+    borderRadius: {
+      regular: "6px",
     },
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".scrollbar-hidden::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".scrollbar-hidden": {
+          "-ms-overflow-style": "none" /* IE and Edge */,
+          "scrollbar-width": "none" /* Firefox */,
+        },
+      };
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
+};
